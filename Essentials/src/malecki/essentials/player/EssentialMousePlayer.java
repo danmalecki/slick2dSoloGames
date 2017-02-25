4d767d752df3cb3e35dfa52ca2b325da78cec2ac
Daniel Malecki <Daniel Malecki@10.0.0.133>
2017-02-25 13:01:34 -0500

    adding common mouse gameplay

---
+++ Essentials/src/malecki/essentials/player/EssentialMousePlayer.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) Feb 25, 2017 Daniel E. Malecki
+ * All rights reserved.
+ */
+package malecki.essentials.player;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+
+/**
+ * TODO: Javadoc
+ *
+ * @author dmalecki
+ */
+public class EssentialMousePlayer extends EssentialPlayer {
+
+	/**
+	 * TODO: Javadoc
+	 *
+	 * @param ORIGx1
+	 * @param ORIGy1
+	 * @param ORIGx2
+	 * @param ORIGy2
+	 * @throws SlickException
+	 */
+	public EssentialMousePlayer(float ORIGx1, float ORIGy1, float ORIGx2, float ORIGy2)
+			throws SlickException {
+		super(ORIGx1, ORIGy1, ORIGx2, ORIGy2);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void checkInput(GameContainer gc) {
+		setX1(gc.getInput().getMouseX() - getHeight() / 2);
+		setY1(gc.getInput().getMouseY() - getWidth() / 2);
+		gc.setMouseGrabbed(true);
+	}
+}
